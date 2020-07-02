9f3b60548cdf22d0494a58d41118e90098d8a2d8
Neo-Ryo <marco.sch4064@gmail.com>
2020-07-02 23:09:14 +0200

    created the switcher, imported the switches css and font from google

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import StartGame from "./Start";
 import Register from "./Register";
 import GameBoard from "./GameBoard";
@@ -7,31 +7,11 @@ import GameBoard from "./GameBoard";
 export default function MyRouter() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/game-board">Game</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/game-board" component={GameBoard} />
-
-          <Route path="/register" component={Register} />
-
-          <Route exact path="/" component={StartGame} />
-        </Switch>
-      </div>
+      <Switch>
+        <Route path="/game-board" component={GameBoard} />
+        <Route path="/register" component={Register} />
+        <Route exact path="/" component={StartGame} />
+      </Switch>
     </Router>
   );
 }
--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,9 +1,14 @@
-import React from "react";
-
+import React, { useState } from "react";
+import style from "../css/start.module.css";
 export default function StartGame() {
+  const [toggle, settoggle] = useState(false);
   return (
-    <div>
-      <h1>This is home mother fucker!</h1>
+    <div className={style.wrapper}>
+      <h1 className={style.title}>The Switcher</h1>
+      <label className={style.switch}>
+        <input type="checkbox" onClick={() => settoggle(!toggle)} />
+        <span className={style.slider}></span>
+      </label>
     </div>
   );
 }
