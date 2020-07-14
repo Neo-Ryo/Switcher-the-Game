2afc5307311083a7138f5d5b84d668649b8cc8ec
Neo-Ryo <marco.sch4064@gmail.com>
2020-07-14 20:07:14 +0200

    get user in game-board

--- src/components/levels/LevelOne.jsx
+++ src/components/levels/LevelOne.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import style from "../levels/css/LevelOneToTen.module.css";
 import { Link } from "react-router-dom";
 import { Button } from "reactstrap";
+import Axios from "axios";
+import { url } from "../../urls";
+import { useSelector } from "react-redux";
 
 export default function LevelOne() {
+  const [user, setUser] = useState();
+  const [isLoading, setIsLoading] = useState(true);
   const [isSolved, setisSolved] = useState(false);
 
   //those states are in game states
@@ -11,22 +16,31 @@ export default function LevelOne() {
   const [switchTwo, setswitchTwo] = useState(true);
   const [switchThree, setswitchThree] = useState(false);
 
-  if (switchOne && switchTwo && switchThree) {
-    setTimeout(() => {
-      setisSolved(true);
-    }, 1000);
-  }
+  const { uuid } = user;
 
-  if (isSolved) {
-    return (
-      <div>
-        <h1>CONGRATULATION!</h1>
-        <Link to="/game-board">
-          <Button>Back to dashboard</Button>
-        </Link>
-      </div>
-    );
-  }
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        setIsLoading(true);
+        const res = await Axios.get(`${url}/users/${uuid}`);
+        setUser(res.data);
+        console.log("coucou", res.data, uuid, `${url}/users/${uuid}`);
+      } catch (error) {
+        alert("Problems! ReLogin please!");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getUser();
+  }, []);
+
+  const levelUp = async () => {
+    try {
+      await Axios.post(`${url}/users/${uuid}/level`);
+    } catch (error) {
+      alert("something bad happened");
+    }
+  };
 
   const switchOneTrick = () => {
     setswitchOne(!switchOne);
@@ -44,6 +58,22 @@ export default function LevelOne() {
     setswitchThree(!switchThree);
   };
 
+  if (switchOne && switchTwo && switchThree) {
+    setTimeout(() => {
+      setisSolved(true);
+    }, 1000);
+  }
+
+  if (isSolved) {
+    return (
+      <div>
+        <h1>CONGRATULATION!</h1>
+        <Link to="/game-board">
+          <Button>Back to dashboard</Button>
+        </Link>
+      </div>
+    );
+  }
   return (
     <div className={style.wrapper}>
       <h1 className={style.title}>Level 1</h1>
