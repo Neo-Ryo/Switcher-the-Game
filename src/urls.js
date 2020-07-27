4d8c7155ed82313b290bc7bed96967c3bec67e4b
Neo-Ryo <marco.sch4064@gmail.com>
2020-07-27 17:38:29 +0200

    url changed

--- src/urls.js
+++ src/urls.js
@@ -1,2 +1,3 @@
-export const url = "http://localhost:8000";
+// export const url = "http://localhost:8000";
+export const url = "https://switcher-fr-api.herokuapp.com";
 //url API https://switcher-fr-api.herokuapp.com/
