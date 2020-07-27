ba5dd3e4f284cb2b00921ba7aab47ac733bd1093
Neo-Ryo <marco.sch4064@gmail.com>
2020-07-27 09:15:40 +0200

    modal score ok

--- src/components/ModalScore.jsx
+++ src/components/ModalScore.jsx
@@ -15,7 +15,27 @@ const ModalScore = ({ users }) => {
   const toggle = () => setModal(!modal);
   const Users = users;
   const nbrUsers = Users.length;
-  const lvl1 = Users.filter((user) => user.level === 1);
+  const lvl1 =
+    (Users.filter((user) => user.level === 2).length / nbrUsers) * 100;
+  const lvl2 =
+    (Users.filter((user) => user.level === 3).length / nbrUsers) * 100;
+  const lvl3 =
+    (Users.filter((user) => user.level === 4).length / nbrUsers) * 100;
+  const lvl4 =
+    (Users.filter((user) => user.level === 5).length / nbrUsers) * 100;
+  const lvl5 =
+    (Users.filter((user) => user.level === 6).length / nbrUsers) * 100;
+  const lvl6 =
+    (Users.filter((user) => user.level === 7).length / nbrUsers) * 100;
+  const lvl7 =
+    (Users.filter((user) => user.level === 8).length / nbrUsers) * 100;
+  const lvl8 =
+    (Users.filter((user) => user.level === 9).length / nbrUsers) * 100;
+  const lvl9 =
+    (Users.filter((user) => user.level === 10).length / nbrUsers) * 100;
+  const lvl10 =
+    (Users.filter((user) => user.level === 11).length / nbrUsers) * 100;
+  console.log(nbrUsers);
   console.log(lvl1);
   return (
     <div>
@@ -37,52 +57,52 @@ const ModalScore = ({ users }) => {
               <tr>
                 <th scope="row">1</th>
                 <td>LVL 1</td>
-                <td>%</td>
+                <td>{lvl1}%</td>
               </tr>
               <tr>
                 <th scope="row">1</th>
                 <td>LVL 2</td>
-                <td>%</td>
+                <td>{lvl2}%</td>
               </tr>
               <tr>
                 <th scope="row">1</th>
                 <td>LVL 3</td>
-                <td>%</td>
+                <td>{lvl3}%</td>
               </tr>
               <tr>
                 <th scope="row">1</th>
                 <td>LVL 4</td>
-                <td>%</td>
+                <td>{lvl4}%</td>
               </tr>
               <tr>
                 <th scope="row">1</th>
                 <td>LVL 5</td>
-                <td>%</td>
+                <td>{lvl5}%</td>
               </tr>
               <tr>
                 <th scope="row">1</th>
                 <td>LVL 6</td>
-                <td>%</td>
+                <td>{lvl6}%</td>
               </tr>
               <tr>
                 <th scope="row">1</th>
                 <td>LVL 7</td>
-                <td>%</td>
+                <td>{lvl7}%</td>
               </tr>
               <tr>
                 <th scope="row">1</th>
                 <td>LVL 8</td>
-                <td>%</td>
+                <td>{lvl8}%</td>
               </tr>
               <tr>
                 <th scope="row">1</th>
                 <td>LVL 9</td>
-                <td>%</td>
+                <td>{lvl9}%</td>
               </tr>
               <tr>
                 <th scope="row">1</th>
                 <td>LVL 10</td>
-                <td>%</td>
+                <td>{lvl10}%</td>
               </tr>
             </tbody>
           </Table>
